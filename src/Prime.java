7e61c67e060f405c5d8ace13193ad6b332fb409f
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-04-29 13:18:04 +0430

    Prime.java is changed in master branch

--- src/Prime.java
+++ src/Prime.java
@@ -4,39 +4,28 @@ public class Prime {
 
 	public static void main(String[] args) {
 
-		int a = 0;
-		int b = 1;
+		int a = 1350;
+		int b = 111;
 		Integer r = 0;
 		Scanner scan = new Scanner(System.in);
 
-<<<<<<< HEAD
 		System.out.println("Enter an num:");
-=======
+
 		System.out.println("Hey, Enter an num:");
->>>>>>> seo_title
+
 		a = scan.nextInt();
 
 		if (a < 1) {
 			System.out.println("Are you kidding me :D");
 		} else if (a == 1) {
 			System.out.println("Number is prime");
-		} else {
-			b = a - 1;
-			boolean isNumberPrime = true;
-			while (b > 1) {
-				r = a % b;
-				if (r == 0) {
-					isNumberPrime = false;
-					break;
-				}
-				b--;
-			}
-			if (isNumberPrime) {
-				System.out.println("Number is prime");
-			} else {
-				System.out.println("Number is not prime at all you idiot");
-			}
 		}
+
+		if (isNumberPrime) {
+				System.out.println("Number is prime muggle");
+			} else {
+				System.out.println("Number is not prime at all madblood");
+			}
 
 	}
 }
